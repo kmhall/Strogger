1aae0fa58f18fbd2af938bf3d4d0323c52d2074c
Kyle Hall <kylehall484@gmail.com>
2020-03-01 14:39:54 -0600

    Home Page template and new run redirect

--- app/src/main/java/com/strogger/strogger/HomeActivity.java
+++ app/src/main/java/com/strogger/strogger/HomeActivity.java
@@ -1,13 +1,16 @@
 package com.strogger.strogger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AccountActivity {
+public class HomeActivity extends AccountActivity implements View.OnClickListener{
 
+    private Button newRunButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,20 @@ public class HomeActivity extends AccountActivity {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.home, null, false);
         super.dl.addView(contentView, 0);
+
+
+        newRunButton = findViewById(R.id.new_run_button);
+        newRunButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.new_run_button:
+                startActivity(new Intent(HomeActivity.this, RunCountdownActivity.class));
+        }
+    }
+
+
 }
