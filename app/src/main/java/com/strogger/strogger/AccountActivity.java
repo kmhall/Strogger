d79eb60a4fbc484b079db8cda44206e5fea27e94
Jordan Mitchell <jordan-mitchell-1@uiowa.edu>
2020-03-01 22:58:42 -0600

    Created a current run page

--- app/src/main/java/com/strogger/strogger/AccountActivity.java
+++ app/src/main/java/com/strogger/strogger/AccountActivity.java
@@ -1,13 +1,13 @@
 package com.strogger.strogger;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.navigation.NavigationView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/com/strogger/strogger/CurrentRunActivity.java
+++ app/src/main/java/com/strogger/strogger/CurrentRunActivity.java
@@ -1,14 +1,80 @@
 package com.strogger.strogger;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class CurrentRunActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private boolean running;
+
+    private DatabaseReference mDatabase;
+
+    private long lastPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.current_run);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        FloatingActionButton mChronometerButton = findViewById(R.id.chronometer_button);
+        mChronometerButton.setImageResource(R.drawable.ic_pause);
+        mChronometerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeChronometer();
+            }
+        });
+
+        Button mRunButton = findViewById(R.id.end_run_button);
+        mRunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeToFirebase();
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.start();
+        running = true;
+    }
+
+    public void changeChronometer() {
+        FloatingActionButton mChronometerButton = findViewById(R.id.chronometer_button);
+        if(!running){
+            chronometer.setBase(chronometer.getBase() + SystemClock.elapsedRealtime() - lastPause);
+            chronometer.start();
+            running = true;
+            mChronometerButton.setImageResource(R.drawable.ic_pause);
+        } else{
+            lastPause = SystemClock.elapsedRealtime();
+            chronometer.stop();
+            running = false;
+            mChronometerButton.setImageResource(R.drawable.ic_play_arrow);
+        }
+    }
+
+    public void writeToFirebase() {
+        //mDatabase.child("users").child("IkIpCl7JftPeaw3q1jjAeGODQmg2").child("last").setValue("Mitchell");
+        startActivity(new Intent(CurrentRunActivity.this, HomeActivity.class));
+    }
+
 }
