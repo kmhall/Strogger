6b3e09a210a9cdc42042ddd31590a83ccb4a6fb2
Sommer, Karson <karson-sommer@uiowa.edu>
2020-03-02 13:47:35 -0600

    Detect asynchronous Bluetooth connection changes issued from the phone

--- app/src/main/java/com/strogger/strogger/HomeActivity.java
+++ app/src/main/java/com/strogger/strogger/HomeActivity.java
@@ -8,8 +8,10 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -58,41 +60,48 @@ public class HomeActivity extends AccountActivity implements View.OnClickListene
         } else {
             Log.d(tag, "good device");
             BLE_Message.setText("");
-        }
 
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-            Log.d(tag, "blu off");
-            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
-        }
-        if (!bluetoothAdapter.getBondedDevices().iterator().hasNext()) {
-            Log.d(tag, "nonono");
-            BLE_Message.setText("No paired devices");
-            BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
-        } else {
-            Log.d(tag, "pair found");
-            bluetoothDevice = bluetoothAdapter.getBondedDevices().iterator().next();
-            Log.d(tag, "a");
-            bluetoothGatt = bluetoothDevice.connectGatt(this, false, gattCallback);
-            Log.d(tag, "b");
-            bluetoothGatt.discoverServices();
+            final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+            bluetoothAdapter = bluetoothManager.getAdapter();
+            if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+                Log.d(tag, "blu off");
+                Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+            } else {
+                BLE_Message.setText("ble already on");
+                BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.white));
+            }
 
-            Log.d(tag, "serching");
-            bluetoothGatt.getServices();
-            Log.d(tag,Integer.toString(bluetoothGatt.getServices().size()));
-            //bluetoothGatt.getServices().get(0);
-            Log.d(tag, "x");
-            //bluetoothGattCharacteristic = bluetoothGatt.getServices().get(0).getCharacteristics().get(0);
-            Log.d(tag, "c");
-            //bluetoothGatt.setCharacteristicNotification(bluetoothGattCharacteristic,true);
-        }
+            IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mReceiver, filter);
+            /*
+            if (!bluetoothAdapter.getBondedDevices().iterator().hasNext()) {
+                Log.d(tag, "nonono");
+                BLE_Message.setText("No paired devices");
+                BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+            } else {
+                Log.d(tag, "pair found");
+                bluetoothDevice = bluetoothAdapter.getBondedDevices().iterator().next();
+                Log.d(tag, "a");
+                bluetoothGatt = bluetoothDevice.connectGatt(this, false, gattCallback);
+                Log.d(tag, "b");
+                bluetoothGatt.discoverServices();
 
+                Log.d(tag, "serching");
+                bluetoothGatt.getServices();
+                Log.d(tag, Integer.toString(bluetoothGatt.getServices().size()));
+                //bluetoothGatt.getServices().get(0);
+                Log.d(tag, "x");
+                //bluetoothGattCharacteristic = bluetoothGatt.getServices().get(0).getCharacteristics().get(0);
+                Log.d(tag, "c");
+                //bluetoothGatt.setCharacteristicNotification(bluetoothGattCharacteristic,true);
+            }*/
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_CANCELED) {
             BLE_Message.setText("Bluetooth is turned off");
             BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
@@ -100,7 +109,7 @@ public class HomeActivity extends AccountActivity implements View.OnClickListene
             BLE_Message.setText("Enabled");
             BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
         }
-   }
+    }
 
 
     @Override
@@ -116,6 +125,36 @@ public class HomeActivity extends AccountActivity implements View.OnClickListene
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
             BLE_Message.setText(Integer.toString(characteristic.getIntValue(0,0)));
+        }
+    };
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        BLE_Message.setText("Bluetooth off");
+                        BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_purple));
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        BLE_Message.setText("Turning Bluetooth off...");
+                        BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        BLE_Message.setText("Bluetooth on");
+                        BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        BLE_Message.setText("Turning Bluetooth on...");
+                        BLE_Message.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+                        break;
+                }
+            }
         }
     };
 
