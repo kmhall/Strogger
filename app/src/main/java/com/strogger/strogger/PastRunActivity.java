5f6ad04097af72b538c91d566b0e3c95f355d35e
Sommer, Karson <karson-sommer@uiowa.edu>
2020-04-27 10:39:09 -0500

    I don't know what here is meaningful and what files are junk

---
+++ app/src/main/java/com/strogger/strogger/PastRunActivity.java
@@ -0,0 +1,34 @@
+package com.strogger.strogger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastRunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*ArrayList<Entry> yValues = new ArrayList<>();
+
+        readings = new ArrayList();
+        int[] measurements = {0,10,35,65,65,65,65,70,60,42};
+        int value;
+        //TEMPORARY USING TO POPULATE DATABASE WITH MOCK READINGS
+        for(int i=0; i<10;i++){
+            value = measurements[i];
+            DeviceReading reading = new DeviceReading(value,i);
+            readings.add(reading);
+            yValues.add(new Entry(i,value));
+        }
+
+        LineDataSet set1 = new LineDataSet(yValues, "Newtons (N)");
+
+        set1.setFillAlpha(110);
+        set1.setLineWidth(3f);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+        */
+    }
+}
