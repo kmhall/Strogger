39e8016bc6e207a36419bb23930efa1b90d71107
Kyle Hall <kylehall484@gmail.com>
2020-05-03 10:35:46 -0500

    Profile image can be uploaded from photos and is saved locally

--- app/src/main/java/com/strogger/strogger/GlobalVariables.java
+++ app/src/main/java/com/strogger/strogger/GlobalVariables.java
@@ -1,6 +1,10 @@
 package com.strogger.strogger;
 
+import android.net.Uri;
+
 public class GlobalVariables {
+
+    public static Uri profileImgUri;
     public static boolean bluetoothPopupSwitch = true;
     public static boolean audioPopupSwitch = true;
     public static boolean runningPopupSwitch = true;
